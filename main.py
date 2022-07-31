021d7f6d7998214d5df8126ba81dd00097ce7740
saarger <92447471+saarger@users.noreply.github.com>
2022-07-31 19:36:43 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def city_pet():
+
+    print("welcome to the band name generator")
+    city = input("what's name of the city you grew up in?\n")
+    pet = input("what's your pet's name?\n")
+    print("your band name colud be " + city + " " + pet)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    city_pet()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
